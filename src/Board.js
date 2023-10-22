950b718d518136486272e4ffe6304c923e14fc30
Juan Villa <ing.villa.jimenez@gmail.com>
2023-10-22 13:13:15 -0600

    Displaying the Game Board

--- src/Board.js
+++ src/Board.js
@@ -29,10 +29,20 @@ import "./Board.css";
  **/
 
 class Board extends Component {
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25,
+  };
+
   constructor(props) {
     super(props);
 
     // TODO: set initial state
+    this.state = {
+      hasWon: false,
+      board: this.createBoard(),
+    };
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -40,6 +50,14 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn);
+      }
+      board.push(row);
+    }
+
     return board;
   }
 
@@ -69,10 +87,26 @@ class Board extends Component {
   /** Render game board or winning message. */
 
   render() {
+    console.log(this.state.board);
     // if the game is won, just show a winning msg & render nothing else
     // TODO
     // make table board
     // TODO
+    let tblBoard = [];
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        let coord = `${y}-${x}`;
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]} />);
+      }
+      tblBoard.push(<tr key={y}>{row}</tr>);
+    }
+
+    return (
+      <table className="Board">
+        <tbody>{tblBoard}</tbody>
+      </table>
+    );
   }
 }
 
