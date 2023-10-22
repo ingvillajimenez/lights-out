d5abcc2345ffba3e7d8f90523bff8b931c01a89e
Juan Villa <ing.villa.jimenez@gmail.com>
2023-10-22 14:39:34 -0600

    Updates

---
+++ precache-manifest.40c2f7c0e8507fabfcfb0b91ec3dfd54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cb76ba9317c8bc25769",
+    "url": "/lights-out/static/css/main.18e9f275.chunk.css"
+  },
+  {
+    "revision": "7cb76ba9317c8bc25769",
+    "url": "/lights-out/static/js/main.7cb76ba9.chunk.js"
+  },
+  {
+    "revision": "2a4eb8a055b70f360807",
+    "url": "/lights-out/static/js/1.2a4eb8a0.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "4bc9329adf5cd4c6d1d8e14178efedb7",
+    "url": "/lights-out/index.html"
+  }
+];
